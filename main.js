773bb76d1b8f8c1ba05181f98a10df8ef04a123a
unknown <jaltable@fi.uba.ar>
2024-08-07 00:38:11 -0300

    added background image and orbit controls

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css';
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -16,16 +18,35 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.ConeGeometry(8, 16, 30);
-const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true} );
+const geometry = new THREE.ConeGeometry(8, 16, 15);
+const material = new THREE.MeshStandardMaterial( {color: 0xFF6347} );
 const obunga = new THREE.Mesh( geometry, material );
 
 scene.add(obunga);
 
 renderer.render( scene, camera );
 
+const light = new THREE.PointLight(0xFFFFFF);
+
+light.position.set(5,5,5)
+
+const ambient = new THREE.AmbientLight(0xFFFFFF);
+
+scene.add(light, ambient);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const bgTexture = new THREE.TextureLoader().load('bg.jfif');
+
+scene.background = bgTexture;
+
 function animate(){
     requestAnimationFrame(animate);
+
+    obunga.rotation.y += 0.01;
+
+    controls.update();
+
     renderer.render(scene, camera);
 }
 
